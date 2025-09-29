1f602c48d69c428ea6b0992b8dab953cdaa1b11b
Jakari Braswell <jbraswell594@my.madonna.edu>
2025-09-28 22:36:40 -0400

    Update timed_challenge.py

--- timed_challenge.py
+++ timed_challenge.py
@@ -5,3 +5,15 @@
 #Track a running total of values. If a negative number is added, reset the total to 0.
 #Input: [5, 7, -1, 3, 2]
 #Output: [5, 12, 0, 3, 5]
+def running_total_with_reset(nums):
+    total = 0
+    result = []
+    for n in nums:
+        if n < 0:
+            total = 0
+        else:
+            total += n
+        result.append(total)
+    return result
+
+print(running_total_with_reset([5, 7, -1, 3, 2]))
