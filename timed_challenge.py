fd6a0998ea54ca9e1910dd81b57d6847b3bde7c6
Jakari Braswell <jbraswell594@my.madonna.edu>
2025-09-28 22:32:04 -0400

    Debugged final output issues

--- timed_challenge.py
+++ timed_challenge.py
@@ -1,3 +1,7 @@
 # Pick one question from timed_challenge.txt
 # Paste the question as a comment below
 # Set a timer for 30 minutes and complete the question!
+#Running Total with Reset
+#Track a running total of values. If a negative number is added, reset the total to 0.
+#Input: [5, 7, -1, 3, 2]
+#Output: [5, 12, 0, 3, 5]
